c5ed7a79a802b1065d0ea1286be651c098c6afc1
TheOGKtura <theogktura@gmail.com>
2026-03-28 05:35:30 +0800

    cleanup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const { join } = require('node:path');
 const router = express.Router();
 
-const firebaseAuthController = require('../controllers/authController');
+/*const firebaseAuthController = require('../controllers/authController');
 
 router.post('/api/login', firebaseAuthController.loginUser);
 router.post('/api/logout', firebaseAuthController.logoutUser);
+
+*/
+
+// LOGIN PAGE
+router.get('/', async (req, res) => {
+  res.send('index');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
+});
+
+router.get('/404', (req, res) => {
+  res.render('404');
+});
+
+router.post('/add-test', async (req, res) => {
+  const customers = fb.database.ref().child("test");
+  const primaryKey = "customer_two";
+
+  const res2 = await customers.child(primaryKey).set({
+    "firstName": "John Zel",
+    "lastName": "Bartolo",
+    "location": "new router check"
+  });
+  
+})
 
 module.exports = router;
